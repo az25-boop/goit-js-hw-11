81e6a647652ffe8cf1f96ebabb1119dcc57e35ab
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-07-15 15:30:59 +0300

    css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,19 +1,17 @@
 // render-functions.js
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const gallery = document.querySelector('.gallery');
 
 export function clearGallery() {
-  gallery.innerHTML = '';
+    gallery.innerHTML = '';
 }
 
 export function renderImages(images) {
-  const markup = images
-    .map(
-      image => `
+    const markup = images.map(image => `
         <a href="${image.largeImageURL}" class="gallery__item">
             <img src="${image.webformatURL}" alt="${image.tags}" class="gallery__image"/>
             <div class="info">
@@ -23,25 +21,21 @@ export function renderImages(images) {
                 <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
             </div>
         </a>
-    `
-    )
-    .join('');
+    `).join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
-  const lightbox = new SimpleLightbox('.gallery a', {
-    /* options */
-  });
-  lightbox.refresh();
+    gallery.insertAdjacentHTML('beforeend', markup);
+    const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+    lightbox.refresh();
 }
 
 export function showNotification(message) {
-  iziToast.info({ message, position: 'topRight' });
+    iziToast.info({ message, position: 'topRight' });
 }
 
 export function showLoader() {
-  document.querySelector('.loader').classList.remove('hidden');
+    document.querySelector('.loader').classList.remove('hidden');
 }
 
 export function hideLoader() {
-  document.querySelector('.loader').classList.add('hidden');
+    document.querySelector('.loader').classList.add('hidden');
 }
