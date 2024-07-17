521da912077e6a7850a050c3a1d6a587bc8455eb
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-07-17 16:04:56 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,12 @@ const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
   const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}?key=${API_KEY}
+    &q=${query}
+    &image_type=photo
+    &orientation=horizontal
+    &safesearch=true
+    &per_page=40`
   );
   if (!response.ok) {
     throw new Error('Failed to fetch images');
